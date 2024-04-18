1ef66de07cac252424e3d5d80c4a28bb298a3645
Francis KOUAHO <kouahofrancis@gmail.com>
2024-04-19 01:30:48 +0200

    feat(back): database ok

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -11,6 +11,14 @@ import router from '@adonisjs/core/services/router'
 import { middleware } from '#start/kernel'
 
 const SessionController = () => import('#controllers/session_controller')
+const InvoiceItemsController = () => import('#controllers/invoice_items_controller')
+const InvoicesController = () => import('#controllers/invoices_controller')
+const OrdersController = () => import('#controllers/orders_controller')
+const PaymentsController = () => import('#controllers/payments_controller')
+const PlansController = () => import('#controllers/plans_controller')
+const ProductServicesController = () => import('#controllers/product_services_controller')
+const PublicBusinessDataController = () => import('#controllers/public_business_data_controller')
+const TaxInformationsController = () => import('#controllers/tax_informations_controller')
 
 router
   .group(() => {
@@ -33,5 +41,30 @@ router
           .prefix('profile')
       })
       .prefix('auth')
+
+    router
+      .group(() => {
+        router
+          .group(() => {
+            router.resource('invoice-items', InvoiceItemsController).apiOnly()
+            router.resource('invoices', InvoicesController).apiOnly()
+          })
+          .prefix('billing')
+
+        router
+          .group(() => {
+            router.resource('product-services', ProductServicesController).apiOnly()
+            router.resource('public-business-data', PublicBusinessDataController).apiOnly()
+            router.resource('tax-informations', TaxInformationsController).apiOnly()
+          })
+          .prefix('business-data')
+
+        router.group(() => {
+          router.resource('orders', OrdersController).apiOnly()
+          router.resource('payments', PaymentsController).apiOnly()
+          router.resource('plans', PlansController).apiOnly()
+        })
+      })
+      .use([middleware.auth()])
   })
-  .prefix('api/v1/')
+  .prefix('api/v1')
--- setup.sh
+++ setup.sh
@@ -3,3 +3,21 @@
 docker compose down -v
 
 docker compose up --build -d --remove-orphans
+
+sleep 10
+
+docker exec -it back node ace migration:rollback --batch 0
+
+docker exec -it back node ace migration:run
+
+docker exec -it back node ace db:seed --files "./database/seeders/user_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/client_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/bank_detail_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/invoice_item_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/product_service_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/invoice_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/tax_information_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/public_business_datum_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/plan_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/payment_seeder.ts"
+docker exec -it back node ace db:seed --files "./database/seeders/order_seeder.ts"
