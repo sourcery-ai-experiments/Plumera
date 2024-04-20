f3f9e8497736257d58ad4b4d8a69a624c171e80a
Francis KOUAHO <kouahofrancis@gmail.com>
2024-04-20 17:34:22 +0200

    feat(front): account

--- frontend/src/app/dashboard/compagny/page.tsx
+++ frontend/src/app/dashboard/compagny/page.tsx
@@ -16,7 +16,7 @@ const Page = () => {
     <section className="py-1 bg-blueGray-50 text-black">
       <div className="w-full px-4 mx-auto mt-6">
         <div className="relative flex flex-col break-words w-full mb-6 rounded-lg bg-blueGray-100 border-0">
-          <div className="rounded-t bg-white mb-0 px-6 py-6">
+          <div className="rounded-t mb-0 px-6 py-6">
             <div className="text-center flex justify-between">
               <h6 className="text-blueGray-700 text-xl font-bold">
                 Mon entreprise
