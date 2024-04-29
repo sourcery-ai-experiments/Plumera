087526089afce88ee4d1fcf1451d4d0c3d99c9c9
Francis KOUAHO <kouahofrancis@gmail.com>
2024-04-29 22:53:56 +0200

    feat(front): color

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -1,23 +1,35 @@
 'use client'
 
-import { ReactNode, Suspense } from 'react'
+import { ReactNode, useEffect, useState } from 'react'
 import SideBar from '@/components/organisms/sidebar/sideBar'
 import TopBar from '@/components/organisms/topbar/topBar'
 import AuthContextProvider from '@/context/AuthContext'
 import AuthGuard from '@/lib/AuthGuard'
 
-const Layout = ({ children }: { children: ReactNode }) => (
-  <AuthContextProvider>
-    <AuthGuard>
-      <section className="c-layout-dashboard">
-        <SideBar />
-        <div className="c-layout-dashboard__container">
-          <TopBar />
-          {children}
-        </div>
-      </section>
-    </AuthGuard>
-  </AuthContextProvider>
-)
+const Layout = ({ children }: { children: ReactNode }) => {
+  const [isClient, setIsClient] = useState(false)
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  return (
+    <>
+      {isClient && (
+        <AuthContextProvider>
+          <AuthGuard>
+            <section className="c-layout-dashboard">
+              <SideBar />
+              <div className="c-layout-dashboard__container">
+                <TopBar />
+                {children}
+              </div>
+            </section>
+          </AuthGuard>
+        </AuthContextProvider>
+      )}
+    </>
+  )
+}
 
 export default Layout
